d917409b0b1c29fbd2f17839154bf08b902b28b3
kopelman Yuval <kopelman050@gmail.com>
2021-12-23 18:28:00 +0200

    wip

--- client.c
+++ client.c
@@ -4,12 +4,38 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <sys/stat.h>
 
-void handler1(int signal){
-    printf("Print solution: "); 
+void handler1(int sig){
+    int init_size;
+    char delim[] =  "\n";
+    char *ptr;
+    int arr[4];
+    int k = 0;
+    pid_t pid;
+    char buffer[256];
+    signal(SIGUSR1, handler1);
+    //sleep(1);
+   FILE *fp;
+   FILE* fpServer;
+   int i=0;
+   char ch;
+   fpServer = fopen("to_client.txt", "r"); // read the result from "to_client" file  
+   while((ch = fgetc(fpServer)) != EOF){
+       buffer[i] = ch;
+       i++;
+   }
+   init_size = strlen(buffer);
+    ptr = strtok(buffer, delim);
+      while(ptr != NULL)
+      {
+        arr[k] = atoi(ptr);
+        ptr = strtok(NULL, delim);
+        k++;
+      }
+   printf("The result is: %s\n",arr[2]);
+   kill(arr[1],SIGUSR1);
 }
+  // kill()
 
 int main(int argc, char* argv[]){
    
@@ -25,13 +51,6 @@ int main(int argc, char* argv[]){
    //Open output file and prints the arguments (client pid, var1, signal number, var2)
    fp = fopen ("output.txt","w");
    fprintf(fp,"%s\n%s\n%s\n%s\n", argv[1], argv[2], argv[3], argv[4]);
-   //////////////////////////////////////////////////////////////////////////////////
-    fpServer = fopen("to_client.txt", "r"); // read the result from "to_client" file  
-    while((ch = fgetc(fpServer)) != EOF){
-      buffer[i] = ch;
-      i++;
-   }
-   printf("The result is: %s\n",buffer); 
    //////////////////////////////////////////////////////////////////////////////////
    exit (0);
 
--- server.c
+++ server.c
@@ -5,83 +5,93 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <sys/stat.h>
- 
+
+int result = 0;
 void handler1(int sig){
-    signal(SIGUSR1, handler1);
-
-}
-
-
- int main(int argc, char* argv[]){
-   // signal(SIGUSR1, handler1);
     FILE* fd;
     FILE* fd2;
     char buffer[256];
     char ch;
     int i = 0;
-    int pid; 
+   // int pid; 
     int tmp;
     int init_size;
     char delim[] =  "\n";
     char *ptr;
     int arr[4];
     int k = 0;
-    int result;
+    pid_t pid;
+    signal(SIGUSR1, handler1);
+    /////////////////////////////////////////////////////////////////////////////////////////////
 
-    /////////////////////////////////////////////////////////////////////////////////////////////
-    fd = fopen("output.txt", "r"); //read the arguments sended by the client in "output.txt" file
-    if (fd == NULL)
-    {
-        perror("Error while opening the file.\n");
-        exit(EXIT_FAILURE);
+    pid = fork();
+    if (pid < 0){
+        printf("Fork Error\n");
+        exit(0);
     }
-    while((ch = fgetc(fd)) != EOF){
-        buffer[i] = ch;
-        i++;
-    }
-    /////////////////////////////////////////////////////////////////////////////////////////////
-    init_size = strlen(buffer);
-	ptr = strtok(buffer, delim);
-	while(ptr != NULL)
-	{
-        arr[k] = atoi(ptr);
-		ptr = strtok(NULL, delim);
-        k++;
-	}
-    /////////////////////////////////////////////////////////////////////////////////////////////
-    if(arr[2] == 1){ 
-        result = arr[1] + arr[3];
-    }
-    if(arr[2] == 2){ 
-        result = arr[1] - arr[3];
-    }
-
-    if(arr[2] == 3){
-        if(arr[3] == 0){
-            fd2= fopen("to_client.txt","w");
-            if(fd2<0){
-                perror("cant open: com_file");
-                exit(0);
+    else{
+        if (pid == 0){
+            fd = fopen("output.txt", "r"); //read the arguments sended by the client in "output.txt" file
+            if (fd == NULL)
+            {
+                perror("Error while opening the file.\n");
+                exit(EXIT_FAILURE);
             }
-            fprintf(fd2,"%s", "Cannot divide by zero");
-            exit(-1);
-        }else{
-            result = arr[1] / arr[3];
+            while((ch = fgetc(fd)) != EOF){
+                buffer[i] = ch;
+                i++;
+            }
+            /////////////////////////////////////////////////////////////////////////////////////////////
+            init_size = strlen(buffer);
+            ptr = strtok(buffer, delim);
+            while(ptr != NULL)
+            {
+                arr[k] = atoi(ptr);
+                ptr = strtok(NULL, delim);
+                k++;
+            }
+            /////////////////////////////////////////////////////////////////////////////////////////////
+            if(arr[2] == 1){ 
+                result = arr[1] + arr[3];
+            }
+            if(arr[2] == 2){ 
+                result = arr[1] - arr[3];
+            }
+
+            if(arr[2] == 3){
+                if(arr[3] == 0){
+                    fd2= fopen("to_client.txt","w");
+                    if(fd2<0){
+                        perror("cant open: com_file");
+                        exit(0);
+                    }
+                    fprintf(fd2,"%s", "Cannot divide by zero");
+                    exit(-1);
+                }else{
+                    result = arr[1] / arr[3];
+                }
+            }
+            if(arr[2] == 4){
+                result = arr[1] * arr[3];
+            }            
+            /////////////////////////////////////////////////////////////////////////////////////////////
+            kill(arr[0],SIGUSR1);
         }
-    }
-    if(arr[2] == 4){
-        result = arr[1] * arr[3];
-    }
-    /////////////////////////////////////////////////////////////////////////////////////////////
+    } 
+}
+
+
+ int main(int argc, char* argv[]){
+    signal(SIGUSR1, handler1);
+    printf("result = %d",result);
+    FILE* fd2;
     fd2= fopen("to_client.txt","w");
-	if( fd2<0 )
-	{ 
-		perror("cant open: com_file"); 
-		exit(0);
-	}
-    fprintf(fd2,"%d", result);
-    /////////////////////////////////////////////////////////////////////////////////////////////
+        if( fd2<0 )
+        { 
+            perror("cant open: com_file"); 
+            exit(0);
+        }
+    fprintf(fd2,"%s\n%d",argv[1],result);
+    sleep(5);
     return 0;
 }
